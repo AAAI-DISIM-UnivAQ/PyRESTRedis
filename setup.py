9b57b15320667a71293a690cd3be8879fddb2257
Giovanni De Gasperis <giovanni.degasperis@univaq.it>
2019-11-26 12:28:20 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='pyReestRedis',
+    packages=['pyrestredis'],
+    version='0.2',
+    description='Python RESTful simplified interface to Redis',
+    url='https://github.com/AAAI-DISIM-UnivAQ/PyRESTRedis',
+    author='giodegas',
+    author_email='giovanni@giodegas.it',
+    license='MIT',
+    keywords='RESTFul Redis api',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Services',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+)    
+ 
