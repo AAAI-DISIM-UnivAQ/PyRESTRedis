bc368fd916e23c5059e96079ed6215320ba1f98c
giodegas <giovanni@giodegas.it>
2019-11-28 15:21:05 +0100

    refactor SET as a POST method

--- src/PyrRESTRedis.py
+++ src/PyrRESTRedis.py
@@ -12,11 +12,10 @@
 
 __author__ = 'giodegas'
 
-from flask import Flask
+from flask import Flask, request
 from flask_restful import *
-from flask_restful import Resource
+from flask_restful import reqparse, abort, Api, Resource
 from json import dumps
-
 from redis import StrictRedis
 
 SERVICE_PORT = 8379
@@ -26,9 +25,13 @@ HOST_IP = 'localhost'
 app = Flask(__name__)
 api = Api(app)
 
+parser = reqparse.RequestParser()
+# parser.add_argument('task')
+
 class ServiceDiscovery(Resource):
     def get(self):
         return {'REDIS SERVICES': 'INFO, GET, SET, DEL, EXISTS, PUBLISH, KEYS, SADD, SMEMBERS, SREM, HSET, HGET, HGETALL, LPOP, SELECT, /redis....'}
+
 
 class SetKey(Resource):
     def get(self, key_id, value):
@@ -36,11 +39,22 @@ class SetKey(Resource):
         out = R.set(key_id, value)
         return {'SET': out}
 
+
+class SetKeyPost(Resource):
+    def post(self):
+        parser.add_argument('key_id', type=str)
+        parser.add_argument('data', type=str)
+        args = parser.parse_args()
+        out = R.set(args[0], args[1])
+        return {'SET': out}
+
+
 class GetKey(Resource):
     def get(self, key_id):
         global R
         out = R.get(key_id)
         return {'GET': out}
+
 
 class DelKey(Resource):
     def get(self, key_id):
@@ -48,11 +62,13 @@ class DelKey(Resource):
         out = R.delete(key_id)
         return {'DEL': out}
 
+
 class Info(Resource):
     def get(self):
         global R
         out = R.info()
         return {'INFO': out}
+
 
 class Keys(Resource):
     def get(self, keyPattern):
@@ -60,11 +76,13 @@ class Keys(Resource):
         out = R.keys(keyPattern)
         return {'KEYS': out}
 
+
 class ExistsKey(Resource):
     def get(self, key_id):
         global R
         out = R.exists(key_id)
         return {'EXISTS': out}
+
 
 class Sadd(Resource):
     def get(self, key_id, member):
@@ -72,11 +90,13 @@ class Sadd(Resource):
         out = R.sadd(key_id, member)
         return {'SADD': out}
 
+
 class Smembers(Resource):
     def get(self, key_id):
         global R
         out = list(R.smembers(key_id))
         return {'SMEMBERS': out}
+
 
 class Publish(Resource):
     def get(self, channel_id, message):
@@ -84,17 +104,20 @@ class Publish(Resource):
         out = R.publish(channel_id, message)
         return dumps({'PUBLISH': out})
 
+
 class LIndex(Resource):
     def get(self, key_id, index):
         global R
         out = R.lindex(key_id, index)
         return {'LINDEX': out}
 
+
 class HSet(Resource):
     def get(self, key_id, field, value):
         global R
         out = R.hset(key_id, field, value)
         return {'HSET': out}
+
 
 class HGet(Resource):
     def get(self, key_id, field):
@@ -104,11 +127,13 @@ class HGet(Resource):
             out = False
         return {'HGET': out}
 
+
 class HDel(Resource):
     def get(self, key_id, field):
         global R
         out = R.hdel(key_id, field)
         return {'HDEL': out}
+
 
 class HExists(Resource):
     def get(self, key_id, field):
@@ -116,11 +141,13 @@ class HExists(Resource):
         out = R.hexists(key_id, field)
         return {'HEXISTS': out}
 
+
 class HGetAll(Resource):
     def get(self, key_id):
         global R
         out = R.hgetall(key_id)
         return {'HGETALL': out}
+
 
 class SetMemberRemove(Resource):
     def get(self, set_name, member):
@@ -128,11 +155,13 @@ class SetMemberRemove(Resource):
         out = R.srem(set_name, member)
         return {'SREM': out}
 
+
 class LPop(Resource):
     def get(self, listName):
         global R
         out = R.lpop(listName)
         return {'LPOP': out}
+
 
 class Generic(Resource):
     def get(self, cmd, param1=None, param2=None, param3=None):
@@ -148,13 +177,15 @@ class Generic(Resource):
         out = eval(cmdStr)
         return {cmd.upper(): out}
 
+
 class DBSelect(Resource):
     def get(self, dbNum):
         global R
         R = None
-        R = Redis(db=dbNum)
+        R = StrictRedis(db=dbNum)
         out = 'ok'
         return {'SELECT': out}
+
 
 class FlushDB(Resource):
     def get(self, password):
@@ -185,10 +216,18 @@ def addCommand(function, command, arguments=None):
         urlStrLC += '/' + arguments
     api.add_resource(function, urlStrLC, urlStrUC)
 
+
+@app.route('/set/<string:key_id>', methods=['POST']) #GET requests will be blocked
+def json_set(key_id):
+    global R
+    req_data = request.get_json()
+    out = R.set(key_id, dumps(req_data))
+    return dumps(out)
+
 if __name__ == '__main__':
     R = StrictRedis(REDIS_IP, 6379, charset="utf-8", decode_responses=True)
     addCommand(ServiceDiscovery, '/')
-    addCommand(SetKey, '/set', '<string:key_id>/<string:value>')
+    # addCommand(SetKey, '/set', '<string:key_id>/<string:value>')
     addCommand(GetKey, '/get','<string:key_id>')
     addCommand(Publish, '/publish','<string:channel_id>/<string:message>')
     addCommand(Info, '/info')
@@ -207,6 +246,8 @@ if __name__ == '__main__':
     addCommand(LPop, '/lpop', '<string:listName>')
     addCommand(DBSelect, '/select', '<int:dbNum>')
     addCommand(FlushDB, '/flushdb', '<string:password>')
+
+    #api.add_resource(SetKeyPost, '/set')
 
     # All other Redis command with up to 3 arguments, all lower case
     api.add_resource(Generic, '/redis/<string:cmd>',
